1f662c14e1b4613ebe80f500a71ba031b0284644
Davide Canale <fuzzuca@gmail.com>
2014-01-08 09:26:11 +0100

    First UPLOAD

---
+++ MyGeoCoso/FormBase.Secanti.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using Mate.Graph;
+using info.lundin.math;
+
+namespace MyGeoCoso
+{
+    public partial class FormBase
+    {
+    }
+
+    public class BisezioneParametr
+    {
+    }
+}
