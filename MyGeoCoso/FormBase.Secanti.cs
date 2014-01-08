c96b6b7f924c1fef901a5812cbf539ebba52867e
Davide Canale <fuzzuca@gmail.com>
2014-01-08 11:25:29 +0100

    Aggiunta commento
    Aggiunta commento spiegazione matematica

--- MyGeoCoso/FormBase.Secanti.cs
+++ MyGeoCoso/FormBase.Secanti.cs
@@ -20,3 +20,33 @@ namespace MyGeoCoso
     {
     }
 }
+
+/*////////////////////////////////////////////////////////////////////////////
+//      Questo commento contiene la riflessione per le secanti              //
+//                                                                          //
+//                                                                          //
+//      Data una retta, se sappiamo che r: y=xm+q                           //
+//      allora:                                                             //
+//                                                                          //
+//          0 = xm+q    quindi x = -q/m                                     //
+//                                                                          //
+//      Se partiamo dalla formula della retta passante per 2 punti          //
+//                                                                          //
+//                                                                          //
+//      Y-Y1       X-X1                                                     //
+//     ------  _  ------                                                    //
+//      X2-X1      X2-X1                                                    //
+//                                                                          //
+//                                                                          //
+//      Allora arriviamo a                                                  //
+//                                                                          //
+//                                                                          //
+//              X1(y2-y1)                                                   //
+//             ----------  -Y1                                              //
+//              X2-X1                                                       //
+//      X=-----------------------                                           //
+//                 Y2-Y1                                                    //
+//                -------                                                   //
+//                 X2-x1                                                    //
+//                                                                          //
+////////////////////////////////////////////////////////////////////////////*/
